d71f30c386e8d5901939c01dbf1333040936aa42
AKINORI Ishii <akinori.ishii@toyokumo.co.jp>
2019-12-18 13:15:37 +0900

    init

---
+++ script.js
@@ -0,0 +1,142 @@
+/*
+ * CSV export routine
+ */
+// TODO: CSV export routine
+
+/*
+ * my Random function
+ */
+
+const basestr = "abcdefghigklmnopqrstuvwxyz"
+const baseSTR = "ABCDEFGHIGKLMNOPQRSTUVWXYZ"
+
+function randn (n) {
+  return Math.floor(Math.random() * n)
+}
+function randchar() {
+  return basestr[randn(basestr.length)]
+}
+function randCHAR() {
+  return baseSTR[randn(baseSTR.length)]
+}
+const mailPostfixes = ['.com', '.co.jp']
+function randpostfix() {
+  return mailPostfixes[randn(mailPostfixes.length)]
+}
+const countries = ['Japan', 'US', 'UK', 'China', 'Korea', 'Rossia']
+function randCountry() {
+  return countries[randn(countries.length)]
+}
+
+/*
+ * Generate Data Functions
+ */
+const genNum = function (max) {
+  return randn(max)
+}
+
+const genStr = function (max) {
+  let out = ''
+  for (let i = 0; i < randn(5) + 1; i++) {
+    out += randchar()
+  }
+  return out
+}
+
+const genName = function (max) {
+  let out = randCHAR()
+  for (let i = 0; i < randn(5) + 3; i++) {
+    out += randchar()
+  }
+  return out
+}
+
+const genMail = function (max) {
+  return genStr(max) + '@' + genStr(max) + randpostfix()
+}
+
+const genCountry = function (max) {
+  return randCountry()
+}
+
+/*
+ * GLOBAL INFO
+ */
+const info = {
+  head       : ['id', 'name', 'country', 'mail'],
+  constraint : [genNum, genName, genCountry, genMail],
+  dataNum    : 10000,
+  style      : 'csv',
+  len        : function() { return this.head.length }
+}
+
+/*
+ * MAIN
+ */
+function generateHead (str) {
+  for (let key = 0; key < info.len(); key++) {
+    str += info.head[key]
+    if(key !== info.len() - 1) {
+      str += ','
+    }
+    else {
+      str += '\n'
+    }
+  }
+  return str
+}
+function generateDatum (str) {
+  for (let key = 0; key < info.len(); key++) {
+    str += info.constraint[key](info.dataNum)
+    if (key !== info.len() - 1) {
+      str += ','
+    }
+    else {
+      str += '\n'
+    }
+  }
+  return str
+}
+function generateData (str) {
+  for (let cnt = 0; cnt < info.dataNum; cnt++) {
+    for (let key = 0; key < info.len(); key++) {
+      str += info.constraint[key](info.dataNum)
+      if (key !== info.len() - 1) {
+        str += ','
+      }
+      else {
+        str += '\n'
+      }
+    }
+  }
+  return str
+}
+
+const bbb = (i) => {
+  return (a) => a+i
+}
+
+console.log(bbb)
+console.log(bbb(1))
+console.log(bbb(1)(2))
+
+
+/*
+ * Show Information
+ */
+function showHeads () {
+  document.write(generateHead(''))
+}
+function showData () {
+  for (let cnt = 0; cnt < info.dataNum; cnt++) {
+    document.write(generateDatum('') + '<br>')
+  }
+}
+
+
+
+
+
+
+
+
